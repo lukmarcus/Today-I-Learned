807aed8220c621bd510252a45af03236818b9b36
Marek Szumny <lukmarcus@gmail.com>
2023-03-04 23:37:34 +0100

    First SQL Query

---
+++ SQL_Essential_Training/4_Composing_Queries/4.2_Query_Commenting.sql
@@ -0,0 +1,7 @@
+--THIS IS A COMMENT
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/04
+Description: The comment
+*/
---
+++ SQL_Essential_Training/4_Composing_Queries/4.3_Query_Composition.sql
@@ -0,0 +1,12 @@
+/*
+Created by: Marek Szumny
+Create date: 2023/03/04
+Description: This query displays all customers first names, last names and email addresses
+*/
+
+SELECT
+	FirstName,
+	LastName,
+	Email
+FROM
+	Customer;
