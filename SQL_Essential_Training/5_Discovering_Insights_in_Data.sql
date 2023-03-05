50cf7d52b4c2241c027088cb866394708b0dcc48
Marek Szumny <lukmarcus@gmail.com>
2023-03-05 22:40:58 +0100

    SQL Chapter 5 done

--- SQL_Essential_Training/5_Discovering_Insights_in_Data.sql
+++ SQL_Essential_Training/5_Discovering_Insights_in_Data.sql
@@ -189,3 +189,74 @@ WHERE
 	DATE(InvoiceDate)>'2010-05-22' AND total<3.00
 ORDER BY
 	InvoiceDate;
+
+	-- 5.8 Logical operator OR
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/05
+Description: Get all invoices where billing city starts with P or starts with D?
+*/
+
+SELECT
+	InvoiceDate,
+	BillingAddress,
+	BillingCity,
+	Total
+FROM
+	Invoice
+WHERE
+	BillingCity LIKE 'P%' OR BillingCity LIKE 'D%'
+ORDER BY
+	InvoiceDate;
+
+	-- 5.9 Brackets and order
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/05
+Description: Get all invoices that are greater than 1.98 from any cities whose name starts with P or starts with D?
+*/
+
+SELECT
+	InvoiceDate,
+	BillingAddress,
+	BillingCity,
+	Total
+FROM
+	Invoice
+WHERE
+	total>1.98 AND (BillingCity LIKE 'P%' OR BillingCity LIKE 'D%')
+ORDER BY
+	InvoiceDate;
+
+	-- 5.10 IF THEN logic with CASE
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/05
+Description: As many customers as possible to spend between $7.00 and $15.00?
+Sales categories:
+Baseline Purchase - Between $0.99 and $1.99
+Low Purchase - Between $2.00 and $6.99
+Target Purchase - Between $7.00 and $15.00
+Top Performer - Above $15.00
+*/
+
+SELECT
+	InvoiceDate,
+	BillingAddress,
+	BillingCity,
+	Total,
+	CASE
+	WHEN Total <2.00 THEN 'Baseline Purchase'
+	WHEN Total BETWEEN 2.00 AND 6.99 THEN 'Low Purchase'
+	WHEN Total BETWEEN 7.00 AND 15.00 THEN 'Target Purchase'
+	ELSE 'Top Performer'
+	END AS PurchaseType
+FROM
+	Invoice
+WHERE
+	PurchaseType='Top Performer'
+ORDER BY
+	BillingCity;
