2b2ec22547eb05defcda7d8ab83d7fd75e97a7ce
Marek Szumny <lukmarcus@gmail.com>
2023-03-06 13:44:06 +0100

    SQL Chapter 6 done & JOIN tables switch

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Today I Learned
 My repository with various stuff I saw and/or learned.
 
-#### 2023/03/04 - 2023/03/05
+#### 2023-03-04 - 2023-03-06
 - [SQL Essential Training](/SQL_Essential_Training)
+
+#### 2023-03-06
+- Youtube: [Can we replace right join with left join](https://www.youtube.com/watch?v=xHoXppyDuxs)
--- SQL_Essential_Training/6_Accessing_Data_from_Multiple_Tables.sql
+++ SQL_Essential_Training/6_Accessing_Data_from_Multiple_Tables.sql
@@ -21,7 +21,7 @@ ORDER BY Customer.CustomerId;
 /*
 Created by: Marek Szumny
 Create date: 2023/03/05
-Description: List all customers with all their invoices
+Description: Join customers with all their invoices
 */
 
 SELECT
@@ -38,3 +38,81 @@ INNER JOIN
 ON
 	i.CustomerId = c.CustomerId
 ORDER BY c.CustomerId;
+
+    -- 6.7 The LEFT JOIN
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/06
+Description: List all invoices with customers data
+*/
+
+SELECT
+	c.LastName,
+	c.FirstName,
+	i.InvoiceId,
+	i.CustomerId,
+	i.InvoiceDate,
+	i.total
+FROM
+	Invoice AS i
+LEFT JOIN
+	Customer AS c
+ON
+	i.CustomerId = c.CustomerId
+ORDER BY c.CustomerId;
+
+    -- 6.8 The RIGHT JOIN
+    -- RIGHT and FULL OUTER JOINs are not currently supported
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/06
+Description: List all customers with invoices data (switched tables)
+*/
+
+SELECT
+	c.LastName,
+	c.FirstName,
+	i.InvoiceId,
+	i.CustomerId,
+	i.InvoiceDate,
+	i.total
+FROM
+	Invoice AS i
+LEFT JOIN
+	Customer AS c
+ON
+	i.CustomerId = c.CustomerId
+ORDER BY c.CustomerId;
+
+    -- 6.10 Joining many tables
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/06
+Description: JOINS on more than two tables | What employees are responsible for the 10 highest individual sales?
+*/
+
+SELECT
+	e.FirstName,
+	e.LastName,
+	e.EmployeeId,
+	c.FirstName,
+	c.LastName,
+	c.SupportRepId,
+	i.CustomerId,
+	i.total
+FROM
+	Invoice AS i
+LEFT JOIN
+	Customer AS c
+ON
+	i.CustomerId = c.CustomerId
+JOIN
+	Employee as e
+ON
+	c.SupportRepId = e.EmployeeId
+ORDER BY
+	i.total DESC
+LIMIT 10;
