1c873e38a4c8a9e1fdf0e05228d79bfe5f12a847
Marek Szumny <lukmarcus@gmail.com>
2023-03-06 15:26:26 +0100

    SQL Chapter 7 done

--- SQL_Essential_Training/7_SQL_Functions.sql
+++ SQL_Essential_Training/7_SQL_Functions.sql
@@ -78,3 +78,52 @@ FROM
 	Customer
 WHERE
 	Country = 'USA';
+
+    -- 7.6 Date functions
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/06
+Description: Calculate the ages of all employees
+*/
+
+SELECT
+	LastName || ' ' || FirstName AS [Employee],
+	strftime('%Y-%m-%d',Birthdate) AS [Birthdate],
+	strftime('%Y-%m-%d') - strftime('%Y-%m-%d',Birthdate) AS Age
+FROM
+	Employee;
+
+    -- 7.7 Aggregate functions
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/06
+Description: Aggregate Functions | What are our all time global sales?
+*/
+
+SELECT
+	SUM(Total) AS [All time global sales],
+	AVG(Total) AS [Average sales],
+	MAX(Total) AS [Maximum sale],
+	MIN(Total) AS [Minimum sale],
+	COUNT(*) AS [Sales Count]
+FROM
+	Invoice;
+
+    -- 7.8 Nesting functions
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/06
+Description: Nesting Functions
+*/
+
+SELECT
+	SUM(Total) AS [All time global sales],
+	ROUND(AVG(Total),2) AS [Average sales],
+	MAX(Total) AS [Maximum sale],
+	MIN(Total) AS [Minimum sale],
+	COUNT(*) AS [Sales Count]
+FROM
+	Invoice;
