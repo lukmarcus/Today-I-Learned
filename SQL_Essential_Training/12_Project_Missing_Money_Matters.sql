772d9535c6e9041c6a2d2efbbcd22f12b0081f13
Marek Szumny <lukmarcus@gmail.com>
2023-03-07 12:56:47 +0100

    SQL Essential Training DONE

--- SQL_Essential_Training/12_Project_Missing_Money_Matters.sql
+++ SQL_Essential_Training/12_Project_Missing_Money_Matters.sql
@@ -113,3 +113,122 @@ FROM
 	Invoice
 GROUP BY
 	[Year];
+
+    -- 12.1c CHALLENGE 3
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/07
+Description: Get a list of employees who exceeded the average transaction amount from sales they generated during 2011 and 2012
+*/
+
+SELECT
+	DISTINCT
+	e.FirstName,
+	e.LastName
+FROM
+	Invoice AS i
+LEFT JOIN
+	Customer AS c
+ON
+	i.CustomerId = c.CustomerId
+JOIN
+	Employee AS e
+ON
+	c.SupportRepId = e.EmployeeId
+WHERE
+	i.total >
+		(SELECT
+			AVG(total)
+		FROM
+			Invoice
+		WHERE
+			InvoiceDate BETWEEN '2011-01-01' AND '2012-12-31');
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/07
+Description: Create a Commission Payout column that displays each employee’s commission based on 15% of the sales transaction amount
+*/
+
+SELECT
+	e.FirstName,
+	e.LastName,
+	sum(i.total)*0.15 AS [Commission Payout]
+FROM
+	Invoice AS i
+LEFT JOIN
+	Customer AS c
+ON
+	i.CustomerId = c.CustomerId
+JOIN
+	Employee AS e
+ON
+	c.SupportRepId = e.EmployeeId
+WHERE
+	i.InvoiceDate BETWEEN '2011-01-01' AND '2012-12-31'
+GROUP BY
+	e.LastName
+ORDER BY
+	sum(i.total)*0.15 DESC;
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/07
+Description: List the customers that the employee identified in the last question
+*/
+
+SELECT
+	DISTINCT
+	c.FirstName,
+	c.LastName
+FROM
+	Invoice AS i
+LEFT JOIN
+	Customer AS c
+ON
+	i.CustomerId = c.CustomerId
+WHERE
+	i.InvoiceDate BETWEEN '2011-01-01' AND '2012-12-31'
+	AND c.SupportRepId = 3
+ORDER BY
+	c.LastName;
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/07
+Description: Which customer made the highest purchase?
+*/
+
+SELECT
+	c.FirstName,
+	c.LastName,
+	i.total
+FROM
+	Invoice AS i
+LEFT JOIN
+	Customer AS c
+ON
+	i.CustomerId = c.CustomerId
+WHERE
+	i.InvoiceDate BETWEEN '2011-01-01' AND '2012-12-31'
+	AND c.SupportRepId = 3
+ORDER BY
+	i.total DESC;
+
+/*
+Created by: Marek Szumny
+Create date: 2023/03/07
+Description: Look at this customer record—do you see anything suspicious?
+*/
+
+SELECT
+	*
+FROM
+	Customer AS c
+JOIN
+	Employee AS e
+ON
+	c.SupportRepId = e.EmployeeId
+WHERE
+	c.LastName = 'Doeein';
